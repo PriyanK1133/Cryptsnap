0c9bad00711531ee85fcbc129f227491033603d4
Priyank <priyankpatel3311@gmail.com>
2024-04-07 09:50:57 +0530

    firstcommit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+const photo = "https://avatars.githubusercontent.com/u/109744222";
+const Footer = () => {
+  return (
+    <Box
+      bgColor={"blackAlpha.900"}
+      color={"whiteAlpha.700"}
+      minH={"48"}
+      px={"16"}
+      py={["16", "8"]}
+    >
+      <Stack direction={["column", "row"]} h={"full"} alignItems="center">
+        <VStack w={"full"} alignItems={["center", "flex-start"]}>
+          <Text fontWeight={"bold"}>About Us</Text>
+          <Text
+            fontSize={"sm"}
+            letterSpacing={"widest"}
+            textAlign={["center", "left"]}
+          >
+            We are the best crypto trading app in wordl, we Provide our guidance
+            at free of cost.
+          </Text>
+        </VStack>
+
+        <VStack>
+          <Avatar boxSize={"28"} mt={["4", "0"]} src={photo} />
+          <Text>CEO</Text>
+        </VStack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
